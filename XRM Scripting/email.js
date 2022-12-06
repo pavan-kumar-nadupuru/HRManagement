ce1f58dafac99a6c410978d9c99379d8da1c14d4
Pavan Kumar Nadupuru <pavankumarnadupuru@outlook.com>
2022-12-06 17:53:08 +0530

    Finished UI validations and boilerplate code for ribbon buttons

--- XRM Scripting/email.js
+++ XRM Scripting/email.js
@@ -3,52 +3,75 @@ if (!HRMGMT) {
     HRMGMT = {};
 }
 
-if (!HRMGMT.Jscripts) {
-    HRMGMT.Jscripts = {};
+if (!HRMGMT.JScripts) {
+    HRMGMT.JScripts = {};
 }
 
 HRMGMT.JScripts.Email = {
-    constants:{
+    constants: {
         APPROVAL_STATUS: "hrmgmt_approvalstatus",
         APPROVAL_STATUS_CODES: {
             "Approved": 122030001,
             "Rejected": 122030002,
             "Waiting": 122030003
         },
-        EyeCheckRequired: "hrmgmt_eyecheckrequired ",
+        EyeCheckRequired: "hrmgmt_eyecheckrequired",
         APPROVER_EMAIL: "hrmgmt_approvalemail",
     },
 
-    onload: function(executionContext) {
+    onload: function (executionContext) {
         var formContext = executionContext.getFormContext();
 
 
-        var approvalStatus = formContext.getAttribute(HRMGMT.JScripts.constants.APPROVAL_STATUS).getValue();
-        if(approvalStatus === HRMGMT.JScripts.constants.APPROVAL_STATUS_CODES.Waiting) {
+        var approvalStatus = formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS).getValue();
+        if (approvalStatus && approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Waiting) {
             // Make the form read only
-            HRMGMT.JScripts.makeFormReadOnly(formContext);
+            HRMGMT.JScripts.Email.makeFormReadOnly(formContext);
         }
 
+        // Make approver email readonly if 4EyeCheckRequired is ‘NO’
+        if (formContext.getAttribute(HRMGMT.JScripts.Email.constants.EyeCheckRequired).getValue() === false) {
+            formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(true);
+        } else {
+            formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(false);
+        }
 
         // Make approver email readonly if 4EyeCheckRequired is ‘NO’ or Approval Status is ‘Waiting’ or ‘Approved’
-        if (formContext.getAttribute(HRMGMT.JScripts.Email.constants.EyeCheckRequired).getValue() === false || approvalStatus === HRMGMT.JScripts.constants.APPROVAL_STATUS_CODES.Waiting || approvalStatus === HRMGMT.JScripts.constants.APPROVAL_STATUS_CODES.Approved) {
+        if (approvalStatus && (approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Waiting || approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Approved)) {
             formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(true);
         }
 
         // Add event handlers
-        formContext.getAttribute(HRMGMT.JScripts.constants.APPROVAL_STATUS).addOnChange((executionContext) => HRMGMT.JScripts.Email.approvalStatusOnChange(executionContext));
+        formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS).addOnChange((executionContext) => HRMGMT.JScripts.Email.approvalStatusOnChange(executionContext));
+        formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).addOnChange((executionContext) => HRMGMT.JScripts.Email.approverEmailOnChange(executionContext));
+        formContext.getAttribute(HRMGMT.JScripts.Email.constants.EyeCheckRequired).addOnChange((executionContext) => HRMGMT.JScripts.Email.eyeCheckRequiredOnChange(executionContext));
     },
 
-    approvalStatusOnChange: function(executionContext) {
+    approvalStatusOnChange: function (executionContext) {
         var formContext = executionContext.getFormContext();
-        var approvalStatus = formContext.getAttribute(HRMGMT.JScripts.constants.APPROVAL_STATUS).getValue();
-        if(approvalStatus === HRMGMT.JScripts.constants.APPROVAL_STATUS_CODES.Waiting) {
+        var approvalStatus = formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS).getValue();
+        if (approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Waiting) {
             // Make the form read only
-            HRMGMT.JScripts.makeFormReadOnly(formContext);
+            HRMGMT.JScripts.Email.makeFormReadOnly(formContext);
+        }
+        var eyeCheckRequired = formContext.getAttribute(HRMGMT.JScripts.Email.constants.EyeCheckRequired).getValue();
+        if (approvalStatus) {
+            if (eyeCheckRequired === false || approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Waiting || approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Approved) {
+                formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(true);
+            }
+            else {
+                formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(false);
+            }
+        } else {
+            if (eyeCheckRequired === false) {
+                formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(true);
+            } else {
+                formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(false);
+            }
         }
     },
 
-    approverEmailOnChange: function(executionContext) {
+    approverEmailOnChange: function (executionContext) {
         var formContext = executionContext.getFormContext();
 
         var approverEmail = formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).getValue();
@@ -56,7 +79,7 @@ HRMGMT.JScripts.Email = {
 
         // Get current user id
         var currentUserId = Xrm.Utility.getGlobalContext().userSettings.userId;
-        
+
         // Retrieve current user details using webapi
         Xrm.WebApi.retrieveRecord("systemuser", currentUserId, "?$select=internalemailaddress").then(
             function success(result) {
@@ -64,7 +87,7 @@ HRMGMT.JScripts.Email = {
                 var currentDomain = currentEmail.split("@")[1];
                 currentDomain = currentDomain.toLowerCase();
 
-                if(approverEmail.split("@")[1].toLowerCase() != currentDomain || approverEmail === currentEmail) {
+                if (approverEmail.split("@")[1].toLowerCase() != currentDomain || approverEmail === currentEmail) {
                     // Show error message
                     Xrm.Navigation.openAlertDialog({
                         text: "Approver email should be of your current domain and should not be the current user's email address."
@@ -72,7 +95,7 @@ HRMGMT.JScripts.Email = {
                     formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setValue(null);
                 }
             },
-            function(error) {
+            function (error) {
                 Xrm.Navigation.openAlertDialog({
                     text: error.message
                 });
@@ -81,12 +104,42 @@ HRMGMT.JScripts.Email = {
 
     },
 
-    makeFormReadOnly: function(formContext) {
+    eyeCheckRequiredOnChange: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+        var eyeCheckRequired = formContext.getAttribute(HRMGMT.JScripts.Email.constants.EyeCheckRequired).getValue();
+        if (eyeCheckRequired === false) {
+            formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(true);
+        } else {
+            formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(false);
+        }
+        var approvalStatus = formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS).getValue();
+        if (approvalStatus) {
+            if (eyeCheckRequired === false || approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Waiting || approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Approved) {
+                formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(true);
+            }
+            else {
+                formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(false);
+            }
+        }
+        // Refresh the ribbon buttons
+        formContext.ui.refreshRibbon(true);
+    },
+
+    makeFormReadOnly: function (formContext) {
         // Make form read-only
-        formContext.ui.controls.forEach(function(control, index) {
+        formContext.ui.controls.forEach(function (control, index) {
             control.setDisabled(true);
         });
+    },
+
+    sendForApprovalClick: function (formContext) {
+        console.log("Button clicked : sendForApprovalClick");
+    },
+    recallClick: function (formContext) {
+        console.log("Button clicked : recallClick");
+    },
+    optInApprovalClick: function (formContext) {
+        console.log("Button clicked : optInApprovalClick");
     }
-    // Make the form read only if Approval Status value is waiting.
 
 };
