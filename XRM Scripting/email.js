fa28a47628746e7f329038c641a30f1f459ffd60
Pavan Kumar Nadupuru <pavankumarnadupuru@outlook.com>
2022-12-07 15:29:26 +0530

    Updated to add button call functionality

--- XRM Scripting/email.js
+++ XRM Scripting/email.js
@@ -17,6 +17,8 @@ HRMGMT.JScripts.Email = {
         },
         EyeCheckRequired: "hrmgmt_eyecheckrequired",
         APPROVER_EMAIL: "hrmgmt_approvalemail",
+        SUBJECT: "subject",
+        DESCRIPTION: "description"
     },
 
     onload: function (executionContext) {
@@ -47,8 +49,10 @@ HRMGMT.JScripts.Email = {
         formContext.getAttribute(HRMGMT.JScripts.Email.constants.EyeCheckRequired).addOnChange((executionContext) => HRMGMT.JScripts.Email.eyeCheckRequiredOnChange(executionContext));
     },
 
-    approvalStatusOnChange: function (executionContext) {
-        var formContext = executionContext.getFormContext();
+    approvalStatusOnChange: function (executionContext, formContext = null) {
+        if (!formContext) {
+            formContext = executionContext.getFormContext();
+        }
         var approvalStatus = formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS).getValue();
         if (approvalStatus === HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Waiting) {
             // Make the form read only
@@ -104,8 +108,10 @@ HRMGMT.JScripts.Email = {
 
     },
 
-    eyeCheckRequiredOnChange: function (executionContext) {
-        var formContext = executionContext.getFormContext();
+    eyeCheckRequiredOnChange: function (executionContext, formContext = null) {
+        if (formContext === null) {
+            formContext = executionContext.getFormContext();
+        }
         var eyeCheckRequired = formContext.getAttribute(HRMGMT.JScripts.Email.constants.EyeCheckRequired).getValue();
         if (eyeCheckRequired === false) {
             formContext.getControl(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).setDisabled(true);
@@ -128,18 +134,105 @@ HRMGMT.JScripts.Email = {
     makeFormReadOnly: function (formContext) {
         // Make form read-only
         formContext.ui.controls.forEach(function (control, index) {
+            // DO NOT disable the approval status field
+            // if (control.getName() !== HRMGMT.JScripts.Email.constants.APPROVAL_STATUS) {
             control.setDisabled(true);
+            // }
         });
     },
 
     sendForApprovalClick: function (formContext) {
         console.log("Button clicked : sendForApprovalClick");
+
+        // Send email to approver
+        var approverEmail = formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).getValue();
+        if (approverEmail) {
+            // Make approval status waiting
+            formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS).setValue(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS_CODES.Waiting);
+
+            // Make a POST call to https://prod-146.westus.logic.azure.com:443/workflows/c19b3c30081044d7a3114eee2de488b3/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2WPJj4ENgzgTNAEH34ny8BCCQu_OYpxfCvcMvRYLM3g with required data
+            var subject = formContext.getAttribute(HRMGMT.JScripts.Email.constants.SUBJECT).getValue();
+            var description = formContext.getAttribute(HRMGMT.JScripts.Email.constants.DESCRIPTION).getValue();
+            var data = {
+                "email": approverEmail,
+                "recalled": "no",
+                "id": formContext.data.entity.getId().replace("{", "").replace("}", ""),
+                "subject": subject || "Approval request",
+                "description": description || "Please take a look at this email and approve or cancel it as you see fit."
+            };
+            var request = new XMLHttpRequest();
+            request.open("POST", "https://prod-77.westus.logic.azure.com:443/workflows/232a4bb50f484ba4b6ae50ca9dd02e6b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bG8UbBEIMwhzYhK_dZt2x5wkI9yM8XJAFaas_wXXLs0", true);
+            request.setRequestHeader("Content-Type", "application/json");
+            request.onreadystatechange = function () {
+                if (request.readyState === 4 && request.status === 200) {
+                    var json = JSON.parse(request.responseText);
+                    console.log(json);
+                }
+            };
+            request.send(JSON.stringify(data));
+
+            HRMGMT.JScripts.Email.approvalStatusOnChange(null, formContext);
+        }
+        else {
+            Xrm.Navigation.openAlertDialog({
+                text: "Approver email is required."
+            });
+            return;
+        }
     },
     recallClick: function (formContext) {
         console.log("Button clicked : recallClick");
+        // Set “Approval Status” as ‘Blank’
+        formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS).setValue(null);
+
+        // Send email to approver
+        var approverEmail = formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVER_EMAIL).getValue();
+        if (approverEmail) {
+            // Make approval status blank
+            formContext.getAttribute(HRMGMT.JScripts.Email.constants.APPROVAL_STATUS).setValue(null);
+
+            // Make a POST call to https://prod-146.westus.logic.azure.com:443/workflows/c19b3c30081044d7a3114eee2de488b3/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2WPJj4ENgzgTNAEH34ny8BCCQu_OYpxfCvcMvRYLM3g with required data
+            var subject = formContext.getAttribute(HRMGMT.JScripts.Email.constants.SUBJECT).getValue();
+            var description = formContext.getAttribute(HRMGMT.JScripts.Email.constants.DESCRIPTION).getValue();
+            var data = {
+                "email": approverEmail,
+                "recalled": "yes",
+                "id": formContext.data.entity.getId().replace("{", "").replace("}", ""),
+                "subject": subject + " - Recalled" || "Approval request - Recalled",
+                "description": "Please ignore the previous email. This email is to inform you that the request has been recalled. Sorry for the inconvenience."
+            };
+            var request = new XMLHttpRequest();
+            request.open("POST", "https://prod-77.westus.logic.azure.com:443/workflows/232a4bb50f484ba4b6ae50ca9dd02e6b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bG8UbBEIMwhzYhK_dZt2x5wkI9yM8XJAFaas_wXXLs0", true);
+            request.setRequestHeader("Content-Type", "application/json");
+            request.onreadystatechange = function () {
+                if (request.readyState === 4 && request.status === 200) {
+                    var json = JSON.parse(request.responseText);
+                    console.log(json);
+                }
+            };
+            request.send(JSON.stringify(data));
+
+            HRMGMT.JScripts.Email.approvalStatusOnChange(null, formContext);
+        }
+        else {
+            Xrm.Navigation.openAlertDialog({
+                text: "Approver email is required."
+            });
+            return;
+        }
+
+
+
+
+        HRMGMT.JScripts.Email.approvalStatusOnChange(null, formContext);
+
     },
     optInApprovalClick: function (formContext) {
         console.log("Button clicked : optInApprovalClick");
+        // Make eye check required field yes
+        formContext.getAttribute(HRMGMT.JScripts.Email.constants.EyeCheckRequired).setValue(true);
+        // Refresh the form
+        HRMGMT.JScripts.Email.eyeCheckRequiredOnChange(null, formContext);
     }
 
 };
